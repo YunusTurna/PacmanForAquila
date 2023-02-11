ced3f39ab9d8032c592ce7917f0fc7725eb95920
Ahmet Yunus Turna <ahmetyunusturna@hotmail.com>
2023-02-11 04:28:20 +0300

    Player Movement Script Yazıldı

--- PacmanForAquila/Assets/Scripts/PlayerMovement.cs
+++ PacmanForAquila/Assets/Scripts/PlayerMovement.cs
@@ -4,38 +4,107 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField] private float speed;
-    private float movement;
-    private bool yatayP;
+  Rigidbody2D rb;
+  private int movementH , movementV;
+  private int speed;
+  private bool isRight = true;
+  private bool isUp = true;
 
-    Rigidbody2D rb;
-    private void Start() {
-        rb = GetComponent<Rigidbody2D>();
-        speed = 50f;
-        movement = 1;
-    }
+  private bool horizontal = true;
+  
 
-    private void Update() {
-        Yatay();
-        
-    }
 
-    private void Yatay(){
-        rb.velocity = new Vector2(movement * speed , 0);
-        if(Input.GetKeyDown(KeyCode.LeftArrow) == true){
-            movement = -1;
+  
+
+  
+
+
+  void Start()
+  {
+    rb = GetComponent<Rigidbody2D>();
+    movementH = 1;
+    movementV = 1;
+    speed = 5;
+  }
+  
+  
+  
+  
+  
+  private void Update() {
+    if(horizontal == true){
+        HorizontalMovement();
+        if(Input.GetKey(KeyCode.UpArrow) == true || Input.GetKey(KeyCode.DownArrow) == true){
+            horizontal = false;
 
         }
-        else{
-            movement = 1;
-        }
 
-        
-
-        
 
     }
+    else{
+        VerticalMovement();
+        if(Input.GetKey(KeyCode.LeftArrow) == true || Input.GetKey(KeyCode.RightArrow) == true){
+            horizontal = true;
+        }
+    }
+
+    
+    
+    
+  }
+
+
+  private void HorizontalMovement(){
+    rb.velocity = new Vector2(movementH * speed , 0);
 
 
 
+    
+    
+      if(isRight == true){
+        movementH = 1;
+        
+        if(Input.GetKey(KeyCode.LeftArrow) == true){
+            isRight = false;
+            movementH = -1;
+            
+        }
+      }
+      else{
+        movementH = -1;
+        
+        if(Input.GetKey(KeyCode.RightArrow))
+        {
+            isRight = true;
+            movementH = 1;
+            
+        }
+      } 
+
+  }
+  private void VerticalMovement(){
+    rb.velocity = new Vector2(0 , movementV * speed);
+    
+    
+      if(isUp == true)
+      {
+        movementV = 1;
+        
+        if(Input.GetKey(KeyCode.DownArrow) == true){
+            isUp = false;
+            movementV = -1;
+        }
+      }
+      else{
+        movementV = -1;
+        
+        if(Input.GetKey(KeyCode.UpArrow))
+        {
+            isUp = true;
+            movementV = 1;
+        }
+      } 
+
+
+  }
 }
